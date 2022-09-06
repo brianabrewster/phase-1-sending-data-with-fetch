f868f3449ee8bf232c54004c26063c225ff03c52
Briana <briana_brewster4@yahoo.com>
2022-09-06 09:30:19 -0500

    commit tree 586845d8eb80b0b049217e8efee6a331e748c31c

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify({
+        name: "Steve",
+        email: "steve@steve.com",
+      }),
+}
+
+function submitData() {
+    return fetch('http://localhost:3000/users', configurationObject)
+    .then(res => res.json())
+    .then(object => document.querySelector('body').append(object.id))
+    .catch(error => document.querySelector('body').append(error))
+}
+
+
+
+
